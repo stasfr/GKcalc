a6e38ec7b057675c20d3c05979d7244baef6cef8
stas_fr <stas240600@yandex.ru>
2023-08-21 21:48:07 +0300

    change routes

--- components/Menu/Index.vue
+++ components/Menu/Index.vue
@@ -4,7 +4,9 @@
   >
     <div><nuxt-link to="/">Graveyard Keeper</nuxt-link></div>
     <div class="flex w-64 justify-around">
-      <div><nuxt-link to="/calculator">Calculator</nuxt-link></div>
+      <div>
+        <nuxt-link to="/recipe_calculator">Recipe Calculator</nuxt-link>
+      </div>
       <div><nuxt-link to="/add_recipe">Add Recipe</nuxt-link></div>
     </div>
   </div>
